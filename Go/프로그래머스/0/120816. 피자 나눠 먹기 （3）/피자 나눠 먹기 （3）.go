e572639cc9f925b36fe957af3ddb2fe59355ba74
YJH <ybox92@gmail.com>
2025-08-18 15:07:56 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ Go/프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.go
@@ -0,0 +1,13 @@
+func solution(slice int, n int) int {
+	answer := 0
+
+	if slice >= 2 && slice <= 10 && n >= 1 && n <= 100 {
+		if n%slice > 0 {
+			answer = n/slice + 1
+		} else {
+			answer = n / slice
+		}
+	}
+
+	return answer
+}
