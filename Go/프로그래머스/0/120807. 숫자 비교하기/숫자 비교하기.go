4dad5fed4994e168efd1cca9071ea7c8a1bf2f6f
YJH <ybox92@gmail.com>
2025-08-18 15:04:14 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ Go/프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.go
@@ -0,0 +1,13 @@
+func solution(num1 int, num2 int) int {
+	answer := 0
+
+	if num1 >= 0 && num1 <= 10000 && num2 >= 0 && num2 <= 10000 {
+		if num1 == num2 {
+			answer = 1
+		} else {
+			answer = -1
+		}
+	}
+
+	return answer
+}
