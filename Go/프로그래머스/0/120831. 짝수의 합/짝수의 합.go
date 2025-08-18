200c640244d24884896581bdb28e917e8e64574e
YJH <ybox92@gmail.com>
2025-08-18 15:21:09 +0900

    [level 0] Title: 짝수의 합, Time: 0.00 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ Go/프로그래머스/0/120831. 짝수의 합/짝수의 합.go
@@ -0,0 +1,15 @@
+package main
+
+func solution(n int) int {
+	answer := 0
+
+	if n > 0 && n <= 1000 {
+		for i := 1; i <= n; i++ {
+			if i%2 == 0 {
+				answer += i
+			}
+		}
+	}
+
+	return answer
+}
